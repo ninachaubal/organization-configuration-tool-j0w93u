a50cab99e7583458cf5ece76ac018b7e58cb6acd
Nina Chaubal <826592+ninachaubal@users.noreply.github.com>
2025-03-24 18:02:09 -1000

    Add public-samples/organization-configuration-tool-j0w93u/src/web/postcss.config.js

---
+++ src/web/postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {}, // tailwindcss v3.3.3 - Process Tailwind CSS directives
+    autoprefixer: {}, // autoprefixer v10.4.15 - Add vendor prefixes to CSS rules
+  },
+}
